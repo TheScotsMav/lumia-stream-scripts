3f6f5f02f8904a266f52555795a12e12ac554be5
Alan Bunce <thrill.seeking.maverick@gmail.com>
2022-10-11 19:09:30 +0100

    Update command-timer.js

--- command-timer/command-timer.js
+++ command-timer/command-timer.js
@@ -1,15 +1,13 @@
 async function() {
-    const linesThreshold = 10; // Set the number of chat lines between each command
-    const timeBetweenCommands = 1; // Minumum number of minutes between commands
+    const linesThreshold = 3; // Set the number of chat lines between each command
+    const timeBetweenCommands = 0; // Minumum number of minutes between commands
     const commands = ["red", "blue", "green"]; // Put in the commands you want in a ticker here
 
     // DON'T EDIT BELOW THIS LINE
-
     let numLines = await getVariable("number_lines");
     let commandNumber = await getVariable("command_number");
     let lastCommandTime = await getVariable("last_command_time");
 
-    [numLines, commandNumber, lastCommandTime] = await Promise.allSettled([numLines, commandNumber, lastCommandTime])
     const timeNow = Date.now();
 
     if (!numLines) {
@@ -25,15 +23,21 @@ async function() {
     };
 
     numLines++;
+
     if (numLines >= linesThreshold && (timeNow - timeBetweenCommands * 60000) >= lastCommandTime) {
+
         callCommand({ name: commands[commandNumber] })
+
         commandNumber++;
+
         await setVariable({ name: "command_number", value: commandNumber % commands.length });
         await setVariable({ name: "number_lines", value: 0 });
         await setVariable({ name: "last_command_time", value: timeNow });
-    } else {
-        await setVariable({ name: "number_lines", value: numLines });
+
+        done();
     }
+
+    await setVariable({ name: "number_lines", value: numLines });
 
     done();
 }
