461a30cad7f9aed431b0311229e2ebc1b163f17d
Alan Bunce <thrill.seeking.maverick@gmail.com>
2022-10-13 18:23:38 +0100

    Update block-commands.js

--- block-commands/block-commands.js
+++ block-commands/block-commands.js
@@ -8,16 +8,16 @@ async function() {
 
     let sceneList = {};
 
-    if (blockedScenes.includes("{{obs_current_scene}}")) {
+    if (blockedScenes.includes(`{{obs_current_scene}}`)) {
         // If the current scene is in the blockedScenes list, stop the command
         return done({ shouldStop: true });
-    } else if (restrictedScenes.includes("{{obs_current_scene}}")) {
+    } else if (restrictedScenes.includes(`{{obs_current_scene}}`)) {
         // check if there is a scene to switch to, if not, continue the command without passing a variable
         if (!sceneSwitching[0]) {
             return done();
         }
         // If the current scene is in the restrictedScenes list, stop any scene changes
-        sceneSwitching.forEach((scene, sceneNumber) => sceneList[`sw-${sceneNumber}`] = "{{obs_current_scene}}");
+        sceneSwitching.forEach((scene, sceneNumber) => sceneList[`sw-${sceneNumber}`] = `{{obs_current_scene}}`);
     } else {
         // check if there is a scene to switch to, if not, continue the command without passing a variable
         if (!sceneSwitching[0]) {
