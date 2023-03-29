d446cd0192b91a27f7653d9b6516b9c373b196f9
Alan Bunce <thrill.seeking.maverick@gmail.com>
2023-03-29 13:21:41 +0100

    add comments

--- points/apply-points.js
+++ points/apply-points.js
@@ -1,24 +1,38 @@
 async function() {
+  // Set the point rate while live and not live
   const pointRateWhileLive = 10;
   const pointRateWhileNotLive = 0;
 
+  // Get the store, current viewers and if the stream is live
   let [viewersInfo, twitchCurrentViewers, twitchLive] = await Promise.all([getStoreItem("viewersInfo"), getVariable("twitch_current_viewers"), getVariable("twitch_live")])
+  // Split the current viewers into an array
   const currentViewers = twitchCurrentViewers.split(", ")
 
+  // Check if the store exists
+  // Store will not exist if it's the first time any points script is run
+  // or if the store has been reset
   if (!viewersInfo) {
+    // If it doesn't, create it
     viewersInfo = {}
   }
 
+  // Set the current point rate
   const currentRate = twitchLive ? pointRateWhileLive : pointRateWhileNotLive;
 
+  // Loop through the current viewers
   currentViewers.forEach((viewerUsername) => {
+    // Check if the viewer exists in the store
     if (viewersInfo[viewerUsername]) {
+      // Add the points to the viewer
       viewersInfo[viewerUsername].points = viewersInfo[viewerUsername].points ? viewersInfo[viewerUsername].points + currentRate : currentRate;
     } else {
+      // If the viewer doesn't exist in the store, create them
+      // Set their points to the current point rate
       viewersInfo[viewerUsername] = { points: currentRate };
     }
   })
 
+  // Save the store
   await setStore({ name: "viewersInfo", value: viewersInfo });
 
   // Make sure you call done() to avoid memory leaks
--- points/remove-points.js
+++ points/remove-points.js
@@ -1,6 +1,5 @@
 async function() {
-  // Get the current viewers from the twitch_current_viewers variable
-  // Get the message from the message variable
+  // Get the store, current viewers and message
   [viewersInfo, twitchCurrentViewers, message] = await Promise.all([getStoreItem("viewersInfo"), getVariable("twitch_current_viewers"), getVariable("message")]);
   // Split the message into parts
   const messageParts = message.split(" ");
@@ -13,9 +12,9 @@ async function() {
     viewersInfo = {}
   }
 
-  // Get the username and points to add
+  // Get the username and points to remove
   const username = messageParts[0].toLowerCase().replace("@", "");
-  const pointsToAdd = parseInt(messageParts[1])
+  const pointsToRemove = parseInt(messageParts[1])
 
   // Check that the user is in chat
   // If they are not in chat, return
@@ -23,26 +22,28 @@ async function() {
     chatbot({ message: "Invalid name or they are not in chat" });
     return done();
   }
-  // Check that the points to add is a valid number
+  // Check that the points to remove is a valid number
   // If it's not a valid number, return
-  if (!pointsToAdd) {
-    chatbot({ message: "Cannot add points, invalid number" });
+  if (!pointsToRemove) {
+    chatbot({ message: "Cannot remove points, invalid number" });
     return done();
   }
 
   // Check if the user exists in the store
   if (viewersInfo[username]) {
-    // Add the points to the user
-    viewersInfo[username].points = viewersInfo[username].points ? viewersInfo[username].points + pointsToAdd : pointsToAdd;
+    // Check if the user has enough points to remove
+    // If they do, remove the points
+    // If they don't, set their points to 0
+    viewersInfo[username].points = (viewersInfo[username].points && viewersInfo[username].points > pointsToRemove) ? viewersInfo[username].points - pointsToRemove : 0;
   } else {
     // If the user doesn't exist in the store, create them
-    // Set their points to the points to add
-    viewersInfo[username] = { points: pointsToAdd };
+    // Set their points to 0
+    viewersInfo[username] = { points: 0 };
   }
 
   // Save the store and send a message to chat
   await setStore({ name: "viewersInfo", value: viewersInfo });
-  chatbot({ message: `Points added ${username} now has ${viewersInfo[username].points} points` });
+  chatbot({ message: `Points removed from ${username} now has ${viewersInfo[username].points} points` });
 
   // Make sure you call done() to avoid memory leaks
   done();
